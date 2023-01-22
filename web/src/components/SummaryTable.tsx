f4da107b052c22d09703aa2137f1759328d3120e
Marcos <marcoscollachiodi@hotmail.com>
2023-01-22 13:07:48 -0300

    create table

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,49 @@
+import { HabitDay } from "./HabitDay"
+import { generateDatesFromYearBeginning } from './../Utils/generate-dates-from-year-beginning';
+
+const weekDays = [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S',
+]
+
+ const summaryDates = generateDatesFromYearBeginning()
+
+const miniSumaryDatesSize = 18 * 7
+const amountOfDaysToFill = miniSumaryDatesSize - summaryDates.length
+
+export function SummaryTable  () {
+    return(
+        <div className="w-full flex" >
+          <div className="grid grid-rows-7 grid-flow-row ga-3" >
+            {weekDays.map((weekDays, i) => {
+                return (
+                    <div key={`${weekDays}-${i}`} 
+                    className="text-zinc-400 text-xl  h-10 w-10 font-bold flex items-center justify-center" >
+                    {weekDays}
+                </div>
+                ) 
+            })}
+            
+            
+           </div>
+            <div className="grid grid-rows-7 grid-flow-col gap-3 " >
+              {summaryDates.map(date => {
+                return <HabitDay key={date.toString()}/>
+              })}
+              {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) =>{
+                return(
+                    <div key={i}
+                    className="w-10 h-10  bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" 
+                    />
+                )
+              })}
+            </div>
+
+        </div>
+    )
+}
